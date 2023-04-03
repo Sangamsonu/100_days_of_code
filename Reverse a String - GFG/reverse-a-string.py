3f976591c7b978e9721c970b1986e54e69107926
Sangam Sonamjit Biswal <112076252+Sangamsonu@users.noreply.github.com>
2023-04-03 19:36:25 +0530

    Added solution - LeetHub

---
+++ Reverse a String - GFG/reverse-a-string.py
@@ -0,0 +1,17 @@
+#User function Template for python3
+class Solution:
+    def revStr (ob, S):
+        # code here 
+
+        return S[::-1]
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        S = input()
+        
+        ob = Solution()
+        print(ob.revStr(S))
+# } Driver Code Ends

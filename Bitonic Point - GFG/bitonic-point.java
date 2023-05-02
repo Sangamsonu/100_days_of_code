de3fd97628bc32273b1e973a5aed03a92aa7a212
Sangam Sonamjit Biswal <112076252+Sangamsonu@users.noreply.github.com>
2023-05-02 20:38:08 +0530

    Added solution - LeetHub

---
+++ Bitonic Point - GFG/bitonic-point.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().findMaximum(arr, n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Solution {
+    int findMaximum(int[] arr, int n) {
+       int low = 0;
+       int high = n-1;
+       int mid=0;
+       
+       while(low <= high){
+            mid = low + (high-low)/2;
+           
+           if((mid ==0 ||arr[mid] >= arr[mid-1]) && (mid == n-1 ||arr[mid] >= arr[mid+1])){
+               return arr[mid];
+           }else if(arr[mid] < arr[mid+1]){
+               low = mid + 1;
+           }else{
+               high = mid - 1;
+           }
+       }
+      return arr[n - 1];
+    }
+}

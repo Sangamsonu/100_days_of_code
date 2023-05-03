eee8c1c6729e99247f6bffcffc119385b7720463
Sangam Sonamjit Biswal <112076252+Sangamsonu@users.noreply.github.com>
2023-05-03 13:35:34 +0530

    Added solution - LeetHub

---
+++ Min sum formed by digits - GFG/min-sum-formed-by-digits.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            System.out.println(obj.minSum(a, n));
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    
+    public static long minSum(int arr[], int n)
+    {
+        Arrays.sort(arr);
+        long sum1 = 0;
+        long sum2 = 0;
+        int i = 0;
+        
+        while(i < n){
+            sum1 = sum1*10 + arr[i];
+            i++;
+            if(i<n)
+            sum2 = sum2*10 + arr[i];
+            i++;
+        }
+        return sum1 + sum2;
+    }
+}

684e2f8b8ff3081e91200bcfdb9286543a4c7380
Sangam Sonamjit Biswal <112076252+Sangamsonu@users.noreply.github.com>
2023-06-04 21:19:07 +0530

    Added solution - LeetHub

---
+++ Merge Without Extra Space - GFG/merge-without-extra-space.java
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.io.*;
+
+public class Main
+{
+    public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		while(t-->0){
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    int n = Integer.parseInt(inputLine[0]);
+		    int m = Integer.parseInt(inputLine[1]);
+		    long arr1[] = new long[n];
+		    long arr2[] = new long[m];
+		    inputLine = br.readLine().trim().split(" ");
+		    for(int i=0; i<n; i++){
+		        arr1[i] = Long.parseLong(inputLine[i]);
+		    }
+		    inputLine = br.readLine().trim().split(" ");
+		    for(int i=0; i<m; i++){
+		        arr2[i] = Long.parseLong(inputLine[i]);
+		    }
+		    Solution ob = new Solution();
+		    ob.merge(arr1, arr2, n, m);
+		    
+		    StringBuffer str = new StringBuffer();
+		    for(int i=0; i<n; i++){
+		        str.append(arr1[i]+" ");
+		    }
+		    for(int i=0; i<m; i++){
+		        str.append(arr2[i]+" ");
+		    }
+		    System.out.println(str);
+		}
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to merge the arrays.
+     static void swap(long a1[],long a2[],int i,int j){
+        long t=a1[i];
+        a1[i]=a2[j];
+        a2[j]=t;
+   
+    }
+    
+    public static void merge(long a1[], long a2[], int n, int m) {
+        int lt=n-1,rt=0;
+        while(lt>=0 && rt<m){
+            if(a1[lt]>a2[rt]){
+                swap(a1,a2,lt,rt);
+                lt--;rt++;
+            }
+            else{
+                break;
+            }
+        }
+        Arrays.sort(a1);
+        Arrays.sort(a2);
+    }
+}
+    

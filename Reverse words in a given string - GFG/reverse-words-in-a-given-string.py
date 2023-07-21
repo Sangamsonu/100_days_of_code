614939ef618cb5566aff9e61cc37d3f2a3e44235
Sangam Sonamjit Biswal <112076252+Sangamsonu@users.noreply.github.com>
2023-07-21 21:15:54 +0530

    Added solution - LeetHub

---
+++ Reverse words in a given string - GFG/reverse-words-in-a-given-string.py
@@ -0,0 +1,23 @@
+# User function Template for python3
+
+class Solution:
+    
+    #Function to reverse words in a given string.
+    def reverseWords(self,S):
+        res = S.split(".")
+        res.reverse()
+        res1 = '.'.join(res)
+        return res1
+
+#{ 
+ # Driver Code Starts
+# Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        s = str(input())
+        obj = Solution()
+        print(obj.reverseWords(s))
+
+# } Driver Code Ends
